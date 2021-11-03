21daf4067790f6c71729dde7226802277c5542a2
sugizou0215 <sugizou0215@gmail.com>
2021-11-03 13:00:11 +0900

    finish 4

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,14 @@
+
+public class Chapter04 {
+	
+	public static void main(String[] args) {
+        String name;
+        name = "Naoto Sugi";
+        System.out.println(name);
+        name = "Lucky Number is";
+        System.out.println(name);
+        int LUCKY_NUMBER = 7;
+        System.out.println(LUCKY_NUMBER);
+    }
+
+}
