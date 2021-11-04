5de9eba2882de08934c7a2b28cd10c1ff6cbe705
sugizou0215 <sugizou0215@gmail.com>
2021-11-04 13:49:12 +0900

    finish 5

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,25 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+        long long1;
+        long1 = 123456789L;
+        double double1;
+        double1 = 1.23;
+        boolean boolean1;
+        boolean1 = true;
+        System.out.println(long1);
+        System.out.println(double1);
+        System.out.println(boolean1);
+        
+        Integer i = 1;
+        System.out.println(i);
+        Float f = 3.1415f;
+        System.out.println(f);
+        Character c = 'あ';
+        System.out.println(c);
+
+        int[] array = { 0, 1, 2, 3, 4 };
+        System.out.println(array[3]);
+    }
+
+}
