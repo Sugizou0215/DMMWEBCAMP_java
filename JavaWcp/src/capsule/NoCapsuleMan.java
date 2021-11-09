edfbfc3299bc88bc8b284589bbc83520c7f17e9b
sugizou0215 <sugizou0215@gmail.com>
2021-11-09 13:38:01 +0900

    finish 12

---
+++ JavaWcp/src/capsule/NoCapsuleMan.java
@@ -0,0 +1,12 @@
+package capsule;
+
+public class NoCapsuleMan {
+	public String name;
+    public int age;
+
+    public NoCapsuleMan(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+}
