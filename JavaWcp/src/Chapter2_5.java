97489ab4158661e6af8670f7ada1fe8fb731dd1b
sugizou0215 <sugizou0215@gmail.com>
2021-11-19 10:15:29 +0900

    finish2_6

--- JavaWcp/src/Chapter2_5.java
+++ JavaWcp/src/Chapter2_5.java
@@ -1,4 +1,18 @@
 
-public class Chapter2_5 {
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
+public class Chapter2_5 {
+	public static void main(String[] args) {
+		List<Integer> numbers = Arrays.asList(1, -8, 4, 2);
+
+		Collections.sort(
+		    numbers,
+		    (Integer x, Integer y) -> {
+		    	  return Integer.compare(x, y);
+		    });
+
+		System.out.println(numbers);
+	}
 }
